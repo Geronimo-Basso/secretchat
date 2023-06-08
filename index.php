d94fc575d7bf44a069dcea764af28d9e68639634
Geronimo Basso <gerobasso01@gmail.com>
2023-06-08 12:53:41 +0200

    Added create chat and join chat files

--- index.php
+++ index.php
@@ -4,14 +4,14 @@ include 'views/header.php';
 
 <article>
     <div>
-        <p>Welcome to the Secret Chat! <br>
+        <p>Welcome to Secret Chat! <br>
         Here you can chat with your friends without anyone knowing what you are talking about. <br>
         Just create a new chatroom and share the code with your friends!</br>
         When the session is <b>finished</b> all the <b>information will be deleted.</b> 
         </p>    
     </div>
-    <button class="button"><a href=""><b>Create a chatroom<b></a></button>
-    <button class="button"><a href=""><b>Join a chatroom<b></a></button>
+    <button class="button"><a href="create-chat/sc_create_chat.php"><b>Create a chatroom<b></a></button>
+    <button class="button"><a href="join-chat/sc_join_chat.php"><b>Join a chatroom<b></a></button>
 </article>
 
 <?php 
