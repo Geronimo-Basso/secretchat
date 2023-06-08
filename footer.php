761832b07b862c3d49b5143fafcaaf766f85311a
Geronimo Basso <gerobasso01@gmail.com>
2023-06-08 15:35:42 +0200

    Added create session id

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
         <footer>
             <p> &copy; Copyright <?php echo date('Y'); ?> The Secret Chat. All rights reserved.</p>
+            <p> Made by Geronimo Basso</p>
             <!-- &copy is for creating the © -->
         </footer>
    </body>
--- sc_create_chat.php
+++ sc_create_chat.php
@@ -1,3 +1,25 @@
 <?php 
     include 'header.php';
 ?>
+
+<section>
+    <div>
+        <h1>Session ID: <?php echo generate_session_id()?></h1>
+    </div>
+</section>    
+
+
+<?php 
+
+    include 'footer.php';
+
+    function generate_session_id(){
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $session_id = '';
+        for ($i = 0; $i < 10; $i++) {
+            $session_id .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $session_id;
+    }
+    
