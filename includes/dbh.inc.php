67c5a61124c35fbab34cabac0117673a104e1340
Geronimo Basso <gerobasso01@gmail.com>
2023-06-08 19:39:39 +0200

    Added connection to myPHPAdmin

---
+++ includes/dbh.inc.php
@@ -0,0 +1,24 @@
+<?php
+
+function executeQuery($query){
+    $dsn = "mysql:host=localhost;dbname=secretchat";
+    $dbusername = "root";
+    $dbpassword = "";
+
+    try{
+        //pdo = php data objects
+        $pdo = new PDO($dsn, $dbusername, $dbpassword);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        echo "Connected successfully <br>";
+
+        $statement = $pdo->prepare($query);
+        $statement->execute();
+
+        $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+        
+        return $results;
+    } catch (PDOException $e){
+        echo "Query failed: ".$e->getMessage();
+    }
+}
--- sc_create_chat.php
+++ sc_create_chat.php
@@ -1,10 +1,11 @@
 <?php 
     include 'header.php';
+    include 'includes/dbh.inc.php';
 ?>
 
 <section>
     <div>
-        <h1>Session ID: <?php echo generate_session_id()?></h1>
+        <h1>Session ID: <?php echo generate_session()?></h1>
     </div>
 </section>    
 
@@ -13,13 +14,27 @@
 
     include 'footer.php';
 
-    function generate_session_id(){
+    function generate_session(){
         $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
         $charactersLength = strlen($characters);
         $session_id = '';
+        $timeStarted = date("Y-m-d H:i:s");
+        $time_limit = 10;
+
         for ($i = 0; $i < 10; $i++) {
             $session_id .= $characters[rand(0, $charactersLength - 1)];
         }
-        return $session_id;
-    }
+
+        $query = "INSERT INTO Chat (idSession,timeStarted,timeLimit) VALUES ('$session_id','$timeStarted','$time_limit')";
+        $results = executeQuery($query);
+
+        $query2 = "SELECT * FROM Chat";
+        $results2 = executeQuery($query2);
+        // Process the results
+        foreach ($results2 as $row) {
+            echo $row['idSession'] . "<br>";
+            echo $row['timeStarted'] . "<br>";
+            echo $row['timeLimit'] . "<br>";
+        }
+}
     
