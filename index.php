f270f3396d7f139f38e70088608933be5e86dedf
Geronimo Basso <gerobasso01@gmail.com>
2023-06-08 11:29:12 +0200

    Started adding data

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php 
-
 include 'views/header.php';
+?>
+
+<article>
+    <div>
+        <p>Welcome to the Secret Chat! <br>
+        Here you can chat with your friends without anyone knowing what you are talking about. <br>
+        Just create a new chatroom and share the code with your friends!</br>
+        When the session is <b>finished</b> all the <b>information will be deleted.</b> 
+        </p>    
+    </div>
+    <button><a href="">Create a new chatroom</a></button>
+    <button><a href="">Join a new chatroom</a></button>
+</article>
+
+<?php 
 include 'views/footer.php';
-
-
 ?>
--- views/header.php
+++ views/header.php
@@ -4,8 +4,10 @@
             <title>Secret Chat</title>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1">
-            <div>
-                <h1>Secret Chat</h1>
+            <a  href="https://icons8.com/icon/hnuJKXRIY1PQ/topic-push-notification"></a>
+            <link rel="stylesheet" href="assets/css/style.css" />
+            <div class="header">
+                <img src="assets/visual/secret-chat-logo.svg" alt="secret-chat-logo"/>
             </div>
             
         </head>
